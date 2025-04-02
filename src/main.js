8e08aca97c96404049dfff702cf24b89b53e211d
Daniel Balobalo <70095157+Daniel-Balobalo@users.noreply.github.com>
2025-04-02 18:31:07 +0800

    4/2/2025 Commit
    > Added the files to their respective directories.
    
    > Added main file.

---
+++ src/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js') // Safer way to expose Node.js features
+        }
+    });
+
+    win.loadFile(path.join(__dirname, 'index.html')); // Ensure correct path
+}
+
+// Quit when all windows are closed (except on macOS)
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
